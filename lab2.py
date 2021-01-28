ba1532ca2ac4e2ca624184a9141d1b5328b931d0
EC2 Default User <ec2-user@ip-172-31-3-63.ec2.internal>
2021-01-28 15:58:08 +0000

    my lab2.py file

---
+++ lab2.py
@@ -0,0 +1,46 @@
+'''
+lab 2
+'''
+
+#3.1
+my_name = 'Tom'
+
+print(my_name.upper())
+
+#3.2
+my_id = 123
+
+print(my_id)
+
+#3.3
+#_123=my_id
+
+my_id = your_id=123
+print(your_id)
+print(my_id)
+
+#3.4
+my_id_str='123'
+
+print(my_id_str)
+
+#3.5
+#my_name+my_id
+
+#3.6
+my_name+my_id_str
+
+print(my_name+my_id_str)
+
+#3.7
+my_name*3
+
+print(my_name*3)
+
+#3.8
+print('hello, world. This is my first python string.'.split('.'))
+
+#3.9
+message= "Tom's id is 123"
+
+print(message)
